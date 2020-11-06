4f4ab9efb1fbafd24fb14caeec80e8dacdbaac0e
Thomas Stanger <thomasstanger0@gmail.com>
2020-11-06 12:15:59 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+            var stack = new Stacks();
+            while (true)
+            {
+                Console.WriteLine("Push, Pop or Exit?");
+                var selector = Console.ReadLine();
+
+                switch (selector)
+                {
+                    case "Push":
+                        stack.InputValue();
+                        break;
+                    case "Pop":
+                        Console.WriteLine(stack.Pop());
+                        break;
+                    case "Exit":
+                        throw new Exception("Program Terminated");
+                    default:
+                        Console.WriteLine("Invalid Entry");
+                        continue;
+                }
+            }
+        }
+        
+    }
+}
---
+++ Stacks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stacks
+    {
+        private List<object> _stackObjects = new List<object>();
+
+        public void InputValue()
+        {
+            while (true)
+            {
+                Console.WriteLine("Add an item to the stack: Leave null or whitespace to stop adding to the stack.");
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+                Push(input);
+            }
+        }
+        private void Push(object value)
+        {
+            _stackObjects.Add(value);
+        }
+
+        public object Pop()
+        {
+            var stackHeight = _stackObjects.Count;
+            if (stackHeight == 0)
+                throw new ArgumentOutOfRangeException("Stack is Empty");
+            var popped = _stackObjects[stackHeight - 1];
+            _stackObjects.RemoveAt(stackHeight-1);
+            return popped;
+        }
+    }
+}
