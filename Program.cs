10919376327091a640a058947ea95b5b0539006e
Thomas Stanger <thomasstanger0@gmail.com>
2020-11-06 12:26:59 +0000

    added comments

--- Program.cs
+++ Program.cs
@@ -13,28 +13,31 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            //Encapsulates the "Run" method so that the Main method is not filled with code
             Run();
         }
 
         private static void Run()
         {
+            //Creates a new instance of the Stacks class
             var stack = new Stacks();
             while (true)
             {
                 Console.WriteLine("Push, Pop or Exit?");
+                //Takes an input from the user to decide what method to run from the stacks class
                 var selector = Console.ReadLine();
-
+                //A switch block allows for simple execution of Stacks internal methods and makes the code unbreakable
                 switch (selector)
                 {
                     case "Push":
-                        stack.InputValue();
+                        stack.InputValue(); //Separated the inputting of a value to be pushed into a separate method so that the push method cannot be directly accessed. This also means that the switch block is not cluttered with code.
                         break;
                     case "Pop":
                         Console.WriteLine(stack.Pop());
                         break;
                     case "Exit":
                         throw new Exception("Program Terminated");
-                    default:
+                    default: //This default block makes the code unbreakable
                         Console.WriteLine("Invalid Entry");
                         continue;
                 }
--- Stacks.cs
+++ Stacks.cs
@@ -5,7 +5,7 @@ namespace Stack
 {
     public class Stacks
     {
-        private List<object> _stackObjects = new List<object>();
+        private List<object> _stackObjects = new List<object>(); //Using a list of type object means that any value can be contained within it without throwing an exception
 
         public void InputValue()
         {
@@ -13,16 +13,13 @@ namespace Stack
             {
                 Console.WriteLine("Add an item to the stack: Leave null or whitespace to stop adding to the stack.");
                 var input = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(input))
-                {
-                    break;
-                }
+                if (string.IsNullOrWhiteSpace(input)) break;
                 Push(input);
             }
         }
         private void Push(object value)
         {
-            _stackObjects.Add(value);
+            _stackObjects.Add(value); //Adds any value to the stack
         }
 
         public object Pop()
@@ -31,7 +28,7 @@ namespace Stack
             if (stackHeight == 0)
                 throw new ArgumentOutOfRangeException("Stack is Empty");
             var popped = _stackObjects[stackHeight - 1];
-            _stackObjects.RemoveAt(stackHeight-1);
+            _stackObjects.RemoveAt(stackHeight-1); //RemoveAt is important because Remove would delete the first instance of an item in the stack. Something that we do not want 
             return popped;
         }
     }
